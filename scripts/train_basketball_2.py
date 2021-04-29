023f6130d48ec2126afa3cae03d2acaba466a963
Guan-De Wu <guadewu@gmail.com>
2021-04-29 02:33:52 -0400

    add metric in args

--- scripts/train_basketball_2.py
+++ scripts/train_basketball_2.py
@@ -60,6 +60,7 @@ parser.add_argument('--loader_num_workers', default=4, type=int)
 parser.add_argument('--obs_len', default=8, type=int)
 parser.add_argument('--pred_len', default=8, type=int)
 parser.add_argument('--skip', default=1, type=int)
+parser.add_argument('--metric', default="meter", type=str)
 parser.add_argument("--model", default="baseline", type=str)
 # Optimization
 parser.add_argument('--batch_size', default=128, type=int) #32

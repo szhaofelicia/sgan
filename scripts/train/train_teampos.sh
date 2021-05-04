1f2edf74f375feb40fa77c8018e9ba1b632f4f6a
Guan-De Wu <guadewu@gmail.com>
2021-05-04 12:09:20 -0400

    fix the bug in attentin team pos

--- scripts/train/train_teampos.sh
+++ scripts/train/train_teampos.sh
@@ -1,6 +1,7 @@
-python train_basketball_2.py  \
-  --dataset_name '01.02.2016.PHX.at.SAC.new' \
-  --dataset_dir ../datasets/nba \
+python train_basketball_teampos.py  \
+  --model team_pos \
+  --dataset_name 'nba-cross.s05' \
+  --dataset_dir /home/jnzs1836/Code/baseball-analysis/datasets \
   --output_dir ./results \
   --delim tab \
   --d_type 'local' \
@@ -9,8 +10,10 @@ python train_basketball_2.py  \
   --encoder_h_dim_d 64\
   --decoder_h_dim 32 \
   --embedding_dim 16 \
+  --team_embedding_dim 4 \
+  --pos_embedding_dim 16 \
   --bottleneck_dim 32 \
-  --mlp_dim 64 \
+  --mlp_dim 128 \
   --num_layers 1 \
   --noise_type gaussian \
   --noise_mix_type global \
@@ -18,6 +21,7 @@ python train_basketball_2.py  \
   --l2_loss_weight 1 \
   --batch_norm 0 \
   --dropout 0 \
+  --tp_dropout 0.5 \
   --batch_size 128 \
   --g_learning_rate 1e-3 \
   --g_steps 1 \
@@ -25,10 +29,13 @@ python train_basketball_2.py  \
   --d_steps 2 \
   --checkpoint_every 10 \
   --print_every 50 \
-  --num_iterations 20000 \
+  --num_iterations 40000 \
   --num_epochs 500 \
   --pooling_type 'pool_net' \
   --clipping_threshold_g 1.5 \
   --best_k 10 \
-  --checkpoint_name gan_zara1 \
+  --g_gamma 0.8 \
+  --d_gamma 0.8 \
+  --interaction_activation none \
+  --checkpoint_name cs05.team_pos_attentiontp_v3.6.d0.e16.pe16.te4.tpd5.gg8.dg8.l10 \
   --restore_from_checkpoint 0

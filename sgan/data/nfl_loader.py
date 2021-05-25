27305e3e52f269de415027bdebfae3558da9fe02
Guan-De Wu <guadewu@gmail.com>
2021-05-25 02:48:52 -0400

    fix a bug in nfl scrips.

---
+++ sgan/data/nfl_loader.py
@@ -0,0 +1,24 @@
+from torch.utils.data import DataLoader
+
+# from sgan.data.trajectories import TrajectoryDataset, seq_collate
+from sgan.data.trajectories_nfl_0427 import TrajectoryDataset, seq_collate
+# from sgan.data.trajectories_basketball_cnn import TrajectoryDataset, seq_collate
+# from sgan.data.trajectories_nfl_cnn import TrajectoryDataset, seq_collate
+
+def data_loader(args, path):
+    dset = TrajectoryDataset(
+        path,
+        obs_len=args.obs_len,
+        pred_len=args.pred_len,
+        skip=args.skip,
+        delim=args.delim,
+        metric=args.metric
+    )
+
+    loader = DataLoader(
+        dset,
+        batch_size=args.batch_size,
+        shuffle=True,
+        num_workers=args.loader_num_workers,
+        collate_fn=seq_collate)
+    return dset, loader

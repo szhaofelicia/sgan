309eba4a44589c4c053de397102b7dbc05fb7dd4
Guan-De Wu <guadewu@gmail.com>
2021-05-25 03:40:55 -0400

    fix a bug in cnn models

--- cnn/attention.py
+++ cnn/attention.py
@@ -29,7 +29,7 @@ class AttentionModule(nn.Module):
     Key-Value: Hidden States
     """
 
-    def __init__(self, image_dim, hidden_dim, n_head=16, key_dim=16, value_dim=16, dropout=0.5):
+    def __init__(self, image_dim, hidden_dim, n_head=16, key_dim=16, value_dim=16, dropout=0.5, len_agents=23):
         super(AttentionModule, self).__init__()
 
         self.image_dim = image_dim
@@ -37,6 +37,7 @@ class AttentionModule(nn.Module):
         self.key_dim = key_dim
         self.value_dim = value_dim
         self.n_head = n_head
+        self.len_agents = len_agents
         self.w_qs = nn.Linear(hidden_dim, n_head * key_dim, bias=False)
         self.w_ks = nn.Linear(image_dim, n_head * key_dim, bias=False)
         self.w_vs = nn.Linear(image_dim, n_head * value_dim, bias=False)
@@ -58,15 +59,15 @@ class AttentionModule(nn.Module):
 
         q = hiddens
         residual = q
-        len_hidden = 11
+        len_hidden = self.len_agents
         v = k
         residual = hiddens
-        q = self.w_qs(q).view(batch_size, len_hidden, n_head, key_dim)
+        q = self.w_qs(q).view(batch_size, -1, n_head, key_dim)
         k = self.w_ks(k).view(batch_size, len_image, n_head, key_dim)
         v = self.w_vs(v).view(batch_size, len_image, n_head, value_dim)
         q, k, v = q.transpose(1, 2), k.transpose(1, 2), v.transpose(1, 2)
         q, attn = self.dot_attention(q, k, v)
-        q = q.transpose(1, 2).contiguous().view(batch_size, len_hidden, -1)
+        q = q.transpose(1, 2).contiguous().view(batch_size, self.len_agents, -1)
         q = self.dropout(self.fc(q))
         q += residual
 
